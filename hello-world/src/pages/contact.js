4fad6f792ba37644e919b571b670c895933dea80
Mohamad Kattaa <Mohamad@fabrik19.local>
2020-07-27 15:23:44 +0200

    0-6

---
+++ hello-world/src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+export default function Contact() {
+  return (
+    <Layout>
+      <Link to="/">Home</Link>
+      <Header headerText="Contact" />
+      <p>Send us a message!</p>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </Layout>
+  )
+}
